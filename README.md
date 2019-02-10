cb083bd703af10d5e65e288be52307b2120213b6
madannes <madannes@madannes.net>
2019-02-09 21:12:57 -0500

    Copied over from stackblitz

--- README.md
+++
@@ -1,1 +0,0 @@
-export: JSON.stringify(fundData.map(i => { return { symbol: i.symbol, description: i.description }}))
--- components/LoadingMessage.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export const LoadingMessage = () => (
-  <h1 className="loading">Loading...</h1>
-);
